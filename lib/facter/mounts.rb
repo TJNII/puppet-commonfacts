a671c4623cb6cfd76e986b8bb7f429a65563f59a
Tom Noonan II <root@tjnii.com>
2013-08-02 21:14:26 -0500

    Added code to work around Facter's apparent requirement for lowercase names

--- lib/facter/mounts.rb
+++ lib/facter/mounts.rb
@@ -6,21 +6,32 @@
 
 file = File.new("/etc/fstab", "r")
 mountpoints = Hash.new
+dupetracker = Hash.new
+mountslist  = Array.new
 while (line = file.gets)
   if not line =~ /^\s*#/ and not line =~ /^\s*$/
     data = line.split(/\s+/)
-
+    
     # Don't monitor noauto filesystems
     if not data[3] =~ /noauto/i
       # Filesystem type
       case data[2]
-        when "proc", "swap"
-           next
+      when "proc", "swap"
+        next
+        
+      else
+        mountslist.push(data[1])
 
+        # Facter facts need to be lowercase
+        # downcase the key, and save a duplicate tracker
+        key = data[1].downcase
+        if dupetracker.has_key?(key)
+          dupetracker[key] = true
         else
-           mountpoints[data[1]] = data[2]
+          mountpoints[key] = data[2]
+          dupetracker[key] = false
+        end
       end
-      
     end
   end
 end
@@ -29,15 +40,20 @@ file.close
 # This is an independent loop because I was getting weird
 # off-by-one errors in the main loop above.
 mountpoints.keys.sort.each do |k|
-  Facter.add("mounttype_" + k) do
-    setcode do
-      mountpoints[k]
+  # Only save non-duplicates
+  # Currently relying on the clients to catch when this is omitted.
+  if dupetracker[k] == false
+    Facter.add("mounttype_" + k) do
+      setcode do
+        mountpoints[k]
+      end
     end
   end
 end
 
 Facter.add("mountpoints") do
   setcode do
-    mountpoints.keys.sort.join(',')
+    # mountpoints is not downcased, so mountpoints.keys no longer works
+    mountslist.sort.join(',')
   end
 end
